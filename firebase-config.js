62f7d75c3c074e9c827a61171e755377a9cc7970
assokk <mijisuu@naver.com>
2025-07-03 20:16:55 +0900

    이미지 추가

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCrjkd7x-e2MDoceF1zsGRtKFd-g-XaI3g",
+  authDomain: "introduction-f2d3e.firebaseapp.com",
+  projectId: "introduction-f2d3e",
+  storageBucket: "introduction-f2d3e.appspot.com",
+  messagingSenderId: "666608232841",
+  appId: "1:666608232841:web:646d07964182230ec8d958",
+};
